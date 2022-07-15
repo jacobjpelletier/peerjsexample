f62c3e105554afc73169e4cd622034eb1c28b829
Jacob Pelletier <jacobjpelletier@gmail.com>
2022-07-15 18:20:06 -0400

    video call via tutorial https://www.youtube.com/watch?v=5JTpRCo0e8s

---
+++ react-peerjs/src/components/App.js
@@ -0,0 +1,14 @@
+import logo from '../icons/logo.svg';
+import '../css/App.css';
+import VideoChat from "./VideoChat";
+//import {useState} from 'react';
+
+function App() {
+  return (
+    <div className="App">
+      <VideoChat />
+    </div>
+  );
+}
+
+export default App;
